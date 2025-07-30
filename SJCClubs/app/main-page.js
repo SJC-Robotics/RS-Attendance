1fd2d43662e036d5c23d1f026c89fcd4769e1851
CrisRobateauSJC <1082322403@sjc.bz>
2025-07-29 20:24:34 -0600

    Logo Updated

--- SJCClubs/app/main-page.js
+++ SJCClubs/app/main-page.js
@@ -2,5 +2,6 @@ import { createViewModel } from './main-view-model';
 
 export function onNavigatingTo(args) {
   const page = args.object
+  page.actionBarHidden = true;
   page.bindingContext = createViewModel()
 }
